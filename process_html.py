555801a69015a78cd975d56688c4c1b4e6f5c1c0
Luis Felipe da Silva Messias <40011115+luis-messias@users.noreply.github.com>
2024-12-26 16:28:06 -0300

    Improving WebScrapping from amazon.com (#2)
    * BKP data
    
    * Adding star filter on webscrapping
    
    * Processing data

--- process_html.py
+++ process_html.py
@@ -1,14 +1,38 @@
 
 from bs4 import BeautifulSoup
+import pandas as pd
 
-def process_magalu_html(file_name='data/test.html'):
+def process_magalu_html(file_name='data/test.html', star=0):
     with open(file_name, "r") as file:
         text = file.read()
         soup = BeautifulSoup(text, "lxml")
-        divs = soup.find_all('div', class_="a-size-base a-color-secondary review-date")
-        print(divs[0])
+        text = soup.find_all('span', class_="a-size-base review-text review-text-content")
+        
+        data = []
+        for t in text:
+            item = {
+                "text": t.findChildren("span" , recursive=False)[0].contents[0],
+                "score": star
+            }
+            data.append(item)
+        return data
+    
+scoreList = []
+starsOptions = ["one_star", "two_star", "three_star", "four_star", "five_star"]
+starsMap = {
+    "one_star": 1, 
+    "two_star": 2, 
+    "three_star": 3, 
+    "four_star": 4, 
+    "five_star": 5
+}
+for opt in starsOptions:
+    for pageNumber in range(11):
+        try:
+            file_name = f"data/page_{opt}_{pageNumber}.html"
+            scoreList += (process_magalu_html(file_name, starsMap[opt]))
+        except:
+            pass
 
-#TODO FIX THIS
-for pageNumber in range(1):
-    file_name = f"data/page_{pageNumber}.html"
-    process_magalu_html(file_name)
+df = pd.DataFrame(scoreList)
+df.to_csv("data/dataProcessed.csv")
--- web_scrapping.py
+++ web_scrapping.py
@@ -9,15 +9,15 @@ options.headless = True
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://www.amazon.com.br/Apple-iPhone-15-128-GB/product-reviews/B0CP6CVJSG/ref=cm_cr_getr_d_paging_btm_next_3?ie=UTF8&reviewerType=all_reviews&pageNumber=3")
+print("Please login on amazon.com")
 time.sleep(60)
 
 def save_html_on_file(search_url, file_name='data/test.html'):
     driver.get(search_url)
     html = driver.page_source
     soup = BeautifulSoup(html, "lxml")
-    divs = soup.find_all('div', class_="a-size-base a-color-secondary review-date")
+    divs = soup.find_all('span', class_="a-size-base a-color-secondary review-date")
 
-    print(divs[0])
     if len(divs) < 1:
         return False
     
@@ -26,45 +26,41 @@ def save_html_on_file(search_url, file_name='data/test.html'):
 
     return True
 
-pageNumber = 0
-retry = 0
-maxRetry = 10
+starsOptions = ["one_star", "two_star", "three_star", "four_star", "five_star"]
+for opt in starsOptions:
+    pageNumber = 0
+    retry = 0
+    maxRetry = 10
+    while True:
+        if pageNumber > 10:
+            break
+        if retry > maxRetry:
+            print(f"Max Retry. Last page: {pageNumber}. Retry count {retry}")
+            break
 
-while True:
-    if retry > maxRetry:
-        print(f"Max Retry. Last page: {pageNumber}. Retry count {retry}")
-        break
 
-    search_url = f"https://www.amazon.com.br/Apple-iPhone-15-128-GB/product-reviews/B0CP6CVJSG/ref=cm_cr_getr_d_paging_btm_next_{pageNumber}?ie=UTF8&reviewerType=all_reviews&pageNumber={pageNumber}"
-    # search_url = f"https://www.magazineluiza.com.br/review/220559300/jogo-de-toalhas-de-banho-atlantica-delicata-garden-gengibre-4-pecas/CM/JOTO/?page={pageNumber}&sortType=MOST_RECENT"
-    file_name = f"data/page_{pageNumber}.html"
+        search_url = f"https://www.amazon.com.br/Apple-iPhone-15-128-GB/product-reviews/B0CP6CVJSG/ref=cm_cr_getr_d_paging_btm_next_{pageNumber}?ie=UTF8&reviewerType=all_reviews&pageNumber={pageNumber}&filterByStar={opt}"
+        file_name = f"data/page_{opt}_{pageNumber}.html"
 
-    if os.path.isfile(file_name):
-        print(f"Page {pageNumber} already get, skipping")
-        retry = 0
-        pageNumber += 1
-        continue
+        if os.path.isfile(file_name):
+            print(f"Page {pageNumber} already get, skipping")
+            retry = 0
+            pageNumber += 1
+            continue
 
-    try:
-        divs_found = save_html_on_file(search_url, file_name)
-    except:
-        print(f"Something fail on page number {pageNumber}. Reloading and retrying.")
+        try:
+            divs_found = save_html_on_file(search_url, file_name)
+        except:
+            print(f"Something fail on page number {pageNumber}. Reloading and retrying.")
+            retry += 1
+            time.sleep(1)
+            continue
 
-        # options = Options()
-        # options.headless = True
-        # driver = webdriver.Chrome(options=options)
-        retry += 1
-        time.sleep(1)
-        continue
-
-    if divs_found:
-        print(f"Review found, page number {pageNumber}")
-        retry = 0
-        pageNumber += 1
-        time.sleep(1)
-    else:
-        retry += 1
-        print(f"Review not found. Retrying page number {pageNumber}, retry count {retry}")
-
-while True:
-    pass
+        if divs_found:
+            print(f"Review found, page number {pageNumber}")
+            retry = 0
+            pageNumber += 1
+            time.sleep(1)
+        else:
+            retry += 1
+            print(f"Review not found. Retrying page number {pageNumber}, retry count {retry}")
